d46dfab8eeb057835fd30f10706f092433e55298
hm-louisa <boehm6@hm.edu>
2023-05-02 12:53:14 +0200

    Initial commit

---
+++ src/main/java/project/devOps/DevOpsTesting/DevOpsTestingApplication.java
@@ -0,0 +1,13 @@
+package project.devOps.DevOpsTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/devOps/DevOpsTesting/DevOpsTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package project.devOps.DevOpsTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
