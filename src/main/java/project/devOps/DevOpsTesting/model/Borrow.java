6c4f39119ffd2ef4a4fd2aa0ac9af94076be5c41
boehm6 <128679941+boehm6@users.noreply.github.com>
2023-05-04 09:16:34 +0200

    Verbesserungen

--- src/main/java/project/devOps/DevOpsTesting/model/Borrow.java
+++ src/main/java/project/devOps/DevOpsTesting/model/Borrow.java
@@ -5,25 +5,38 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Table(name = "borrows")
 @Getter
 @Setter
 public class Borrow {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long borrowID;
-    @Column(nullable = false)
-    private LocalDate startDate;
-    @Column(nullable = false)
-    private LocalDate endDate;
-    @ManyToOne
-    private User borrower;
-    @ManyToOne
-    private Book book;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long borrowID;
 
-    public Long getBookId() {
-        return book.getBookID();
-    }
+  @Column(nullable = false)
+  private LocalDate startDate;
+
+  @Column(nullable = false)
+  private LocalDate endDate;
+
+  @ManyToOne private User borrower;
+  @ManyToOne private Book book;
+
+  public Long getBookId() {
+    return book.getBookID();
+  }
+
+  /**
+   * Diese Methode gibt einen String aus den Daten aus.
+   *
+   * @return String des Start und Endes der Ausleihe.
+   */
+  public String getDate() {
+    return startDate.format(DateTimeFormatter.ISO_DATE)
+        + " "
+        + endDate.format(DateTimeFormatter.ISO_DATE);
+  }
 }
