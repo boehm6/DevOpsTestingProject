763adc0333edbab46e72b20120eb319a4dbf1bf7
hm-louisa <boehm6@hm.edu>
2023-05-04 23:32:21 +0200

    JUnittests completed - Version 1

---
+++ src/test/java/project/devOps/DevOpsTesting/controller/IndexControllerTest.java
@@ -0,0 +1,55 @@
+package project.devOps.DevOpsTesting.controller;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import project.devOps.DevOpsTesting.model.Book;
+import project.devOps.DevOpsTesting.model.Borrow;
+import project.devOps.DevOpsTesting.model.User;
+import project.devOps.DevOpsTesting.service.impl.BookServiceImpl;
+import project.devOps.DevOpsTesting.service.impl.UserServiceImpl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class IndexControllerTest {
+
+    IndexController controller;
+
+    BookServiceImpl bookService;
+
+    UserServiceImpl userService;
+
+    @BeforeEach
+    public void setUp() {
+        bookService = mock(BookServiceImpl.class);
+        userService = mock(UserServiceImpl.class);
+        controller = new IndexController(bookService, userService);
+    }
+
+    @Test
+    public void testHome() {
+        IndexController controller = new IndexController(bookService, userService);
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        List<Book> books = Arrays.asList(new Book(), new Book());
+        when(bookService.getAllBooks()).thenReturn(books);
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.view().name("home"))
+                    .andExpect(MockMvcResultMatchers.model().attributeExists("books"))
+                    .andExpect(MockMvcResultMatchers.model().attribute("books", books));
+        } catch (Exception e) {
+            fail("Should not have thrown an exception");
+        }
+    }
+
+}
