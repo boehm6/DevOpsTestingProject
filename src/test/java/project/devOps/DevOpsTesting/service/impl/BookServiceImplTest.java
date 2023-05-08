182863634e65f84eabb1c9943982d20da33c8b5e
boehm6 <128679941+boehm6@users.noreply.github.com>
2023-05-08 21:19:51 +0200

    JUnit5 implemented

--- src/test/java/project/devOps/DevOpsTesting/service/impl/BookServiceImplTest.java
+++ src/test/java/project/devOps/DevOpsTesting/service/impl/BookServiceImplTest.java
@@ -1,16 +1,13 @@
 package project.devOps.DevOpsTesting.service.impl;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import project.devOps.DevOpsTesting.model.Book;
 import project.devOps.DevOpsTesting.model.Borrow;
@@ -19,81 +16,80 @@ import project.devOps.DevOpsTesting.repository.BorrowRepository;
 
 public class BookServiceImplTest {
 
-    private BookServiceImpl bookService;
+  private BookServiceImpl bookService;
 
-    BookRepository bookRepository = mock(BookRepository.class);
+  BookRepository bookRepository = mock(BookRepository.class);
 
-    BorrowRepository borrowRepository = mock(BorrowRepository.class);
+  BorrowRepository borrowRepository = mock(BorrowRepository.class);
 
-    @Before
-    public void setUp() {
-        bookService = new BookServiceImpl(bookRepository, borrowRepository);
-    }
+  @BeforeEach
+  public void setUp() {
+    bookService = new BookServiceImpl(bookRepository, borrowRepository);
+  }
 
-    @Test
-    public void testGetAllBooks() {
-        // Setup
-        Book book = createBook(1L);
-        Book book2 = createBook(2L);
-        List<Book> expectedBooks = List.of(book, book2);
-        when(bookRepository.findAll()).thenReturn(expectedBooks);
+  @Test
+  public void testGetAllBooks() {
+    // Setup
+    Book book = createBook(1L);
+    Book book2 = createBook(2L);
+    List<Book> expectedBooks = List.of(book, book2);
+    when(bookRepository.findAll()).thenReturn(expectedBooks);
 
-        // Execution
-        List<Book> actualBooks = bookService.getAllBooks();
+    // Execution
+    List<Book> actualBooks = bookService.getAllBooks();
 
-        // Verification
-        assertEquals(expectedBooks.size(), actualBooks.size());
-    }
+    // Verification
+    assertEquals(expectedBooks.size(), actualBooks.size());
+  }
 
+  @Test
+  public void testBorrowBook() {
+    // Setup
+    Borrow borrow = new Borrow();
+    Book book = createBook(1L);
+    borrow.setBook(book);
+    when(bookRepository.findById(1L)).thenReturn(java.util.Optional.of(book));
 
+    // Execution
+    bookService.borrowBook(borrow);
 
-    @Test
-    public void testBorrowBook() {
-        // Setup
-        Borrow borrow = new Borrow();
-        Book book = createBook(1L);
-        borrow.setBook(book);
-        when(bookRepository.findById(1L)).thenReturn(java.util.Optional.of(book));
+    // Verification
+    Mockito.verify(borrowRepository).save(borrow);
+    assertFalse(book.isAvailable());
+    Mockito.verify(bookRepository).save(book);
+  }
 
-        // Execution
-        bookService.borrowBook(borrow);
+  @Test
+  public void testGetBookByIdFound() {
+    // Setup
+    Book expectedBook = createBook(1L);
+    when(bookRepository.findAll()).thenReturn(List.of(expectedBook));
 
-        // Verification
-        Mockito.verify(borrowRepository).save(borrow);
-        assertFalse(book.isAvailable());
-        Mockito.verify(bookRepository).save(book);
-    }
+    // Execution
+    Book actualBook = bookService.getBookById(1L);
 
-    @Test
-    public void testGetBookByIdFound() {
-        // Setup
-        Book expectedBook = createBook(1L);
-        when(bookRepository.findAll()).thenReturn(List.of(expectedBook));
+    // Verification
+    assertNotNull(actualBook);
+    assertEquals(expectedBook, actualBook);
+  }
 
-        // Execution
-        Book actualBook = bookService.getBookById(1L);
+  @Test
+  public void testGetBookByIdNotFound() {
+    // Setup
+    when(bookRepository.findAll()).thenReturn(List.of(createBook(1L)));
 
-        // Verification
-        assertNotNull(actualBook);
-        assertEquals(expectedBook, actualBook);
-    }
+    // Execution
+    Book actualBook = bookService.getBookById(2L);
 
-    @Test
-    public void testGetBookByIdNotFound() {
-        // Setup
-        when(bookRepository.findAll()).thenReturn(List.of(createBook(1L)));
+    // Verification
+    assertNull(actualBook);
+  }
 
-        // Execution
-        Book actualBook = bookService.getBookById(2L);
-
-        // Verification
-        assertNull(actualBook);
-    }
-    private Book createBook(long bookId) {
-        Book book = new Book();
-        book.setBookID(bookId);
-        book.setTitle("Test");
-        book.setIsbn("978-456-321");
-        return book;
-    }
+  private Book createBook(long bookId) {
+    Book book = new Book();
+    book.setBookID(bookId);
+    book.setTitle("Test");
+    book.setIsbn("978-456-321");
+    return book;
+  }
 }
