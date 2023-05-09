c22236400f1d4292e49026e75e4c7a03148b6bde
boehm6 <128679941+boehm6@users.noreply.github.com>
2023-05-09 22:45:07 +0200

    improvement

--- src/main/resources/data/import.sql
+++ src/main/resources/data/import.sql
@@ -1,18 +1,19 @@
 INSERT INTO books (isbn, title, author, available)
-VALUES ('9783161484100', 'Der Prozess', 'Franz Kafka', true);
+VALUES
+('9780596517308', 'Continuous Integration: Improving Software Quality and Reducing Risk', 'Paul M. Duvall, Steve Matyas, Andrew Glover', true),
+('9781491938226', 'Site Reliability Engineering: How Google Runs Production Systems', 'Niall Richard Murphy, Betsy Beyer, Chris Jones, Jennifer Petoff', true),
+('9781788996611', 'DevOps with Kubernetes: Accelerating software delivery with container orchestrators', 'Hideto Saito, Hui-Chuan Chloe Lee, Cheng-Yang Wu', true),
+('9781449340049', 'Building Microservices: Designing Fine-Grained Systems', 'Sam Newman', true),
+('9781617292392', 'Docker in Action', 'Jeff Nickoloff', true),
+('9781492033911', 'Kubernetes in Action', 'Marko Luksa', true),
+('9781492056880', 'Cloud Native DevOps with Kubernetes: Building, Deploying, and Scaling Modern Applications in the Cloud', 'John Arundel', true),
+('9781491984300', 'The Site Reliability Workbook: Practical Ways to Implement SRE', 'Betsy Beyer, Niall Richard Murphy, David K. Rensin, Kent Kawahara, Stephen Thorne', true),
+('9780128045417', 'Cloud Computing: Principles and Paradigms', 'Rajkumar Buyya, James Broberg, Andrzej M. Goscinski', true),
+('9781119596579', 'Cloud Computing Design Patterns', 'Thomas Erl, Robert Cope, Amin Naserpour', true);
 
 INSERT INTO books (isbn, title, author, available)
-VALUES ('9783458327403', 'Harry Potter und der Stein der Weisen', 'J.K. Rowling', true);
-
-INSERT INTO books (isbn, title, author, available)
-VALUES ('9783426281545', '1984', 'George Orwell', false);
-
-INSERT INTO books (isbn, title, author, available)
-VALUES ('9783730606920', 'Das Parfum', 'Patrick Süskind', true);
-
-INSERT INTO books (isbn, title, author, available)
-VALUES ('9783423129466', 'Die unendliche Geschichte', 'Michael Ende', true);
-
-INSERT INTO books (isbn, title, author, available)
-VALUES ('9783442457242', 'Der kleine Prinz', 'Antoine de Saint-Exupéry', false);
-
+VALUES
+('9781491976350', 'The DevOps Handbook: How to Create World-Class Agility, Reliability, and Security in Technology Organizations', 'Gene Kim, Jez Humble, Patrick Debois, John Willis', true),
+('9781942788003', 'Continuous Delivery: Reliable Software Releases through Build, Test, and Deployment Automation', 'Jez Humble, David Farley', true),
+('9781449370985', 'Cloud Computing: Concepts, Technology & Architecture', 'Thomas Erl, Ricardo Puttini, Zaigham Mahmood', true),
+('9781119508199', 'Cloud Native Infrastructure: Patterns for Scalable Infrastructure and Applications in a Dynamic Environment', 'Justin Garrison, Kris Nova', true);
